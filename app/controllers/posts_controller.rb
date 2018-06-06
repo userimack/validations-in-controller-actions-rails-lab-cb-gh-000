244b9362adf08dfd0b7194b1a08b7849e963953b
Mahendra Yadav <mahendra.k12@gmail.com>
2018-06-06 11:47:42 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
     
-    if @post.valid?
+    if @post.update(post_params)
       @post.save 
       redirect_to post_path(@post)
     else 
