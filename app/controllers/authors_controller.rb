243c85b5ee8520b487c91597108a4ab8bdde8a97
Mahendra Yadav <mahendra.k12@gmail.com>
2018-06-06 12:25:21 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,16 +4,14 @@ class AuthorsController < ApplicationController
   end
 
   def new
-    @author = Author.new
   end
 
   def create
-    @author = Author.new(author_params)
-
+    @author = Author.create!(author_params)
     if @author.save
       redirect_to author_path(@author)
     else
-      render :new
+      render :new 
     end
   end
 
